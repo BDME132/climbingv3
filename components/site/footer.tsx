0e996557800cb9cf82090d618485e86420ca50bb
ben meilstrup <benmeilstrup@gmail.com>
2026-02-26 13:23:36 -0700

    Update project name and description in package.json; modify footer copyright to reflect new branding

--- components/site/footer.tsx
+++ components/site/footer.tsx
@@ -5,9 +5,7 @@ export const Footer = () => {
     <footer>
       <Section>
         <Container className="text-muted-foreground flex justify-start gap-2 sm:gap-4 items-center">
-          <p>&copy; {new Date().getFullYear()} brijr/mdx</p>
-          <span className="text-muted-foreground/40">|</span>
-          <a href="https://bridgertower.com" className="py-2 inline-block">Bridger Tower</a>
+          <p>&copy; {new Date().getFullYear()} RockClimbUtah</p>
         </Container>
       </Section>
     </footer>
